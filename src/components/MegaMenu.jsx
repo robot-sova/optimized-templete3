519aa8e509940d3a7057e88d54545370b710b65b
robot-sova <6195738869abysov@gmail.com>
2025-02-15 15:28:13 -0800

    1

--- src/components/MegaMenu.jsx
+++ src/components/MegaMenu.jsx
@@ -1,56 +1,100 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./MegaMenu.css";
 
 const MegaMenu = () => {
-  const [isSticky, setSticky] = useState(false);
+  const [activeDropdown, setActiveDropdown] = useState(null);
 
-  useEffect(() => {
-    const handleScroll = () => {
-      setSticky(window.scrollY > 0);
-    };
+  // Handle mouse enter - Lazy Load dropdown only when hovered
+  const handleMouseEnter = (menu) => {
+    setActiveDropdown(menu);
+  };
 
-    window.addEventListener("scroll", handleScroll, { passive: true });
-    return () => window.removeEventListener("scroll", handleScroll);
-  }, []);
+  // Handle mouse leave - Hide dropdown after a small delay
+  const handleMouseLeave = () => {
+    setTimeout(() => setActiveDropdown(null), 200);
+  };
 
   return (
-    <div id="header" className={`navbar ${isSticky ? "fixed shrink" : ""}`}>
-      {/* Logo */}
-      <a className="logo" href="#home">
-        <img alt="Logo" src="/logo.webp" width="157" height="70" />
-      </a>
+    <div id="header" className="sticky">
+      <div className="navbar">
+        {/* Logo */}
+        <a className="logo" href="#home">
+          <img alt="Logo" src="/logo.webp" width="157" height="70" />
+        </a>
 
-      {/* Home Dropdown */}
-      <div className="dropdown">
-        <button className="dropbtn">Home</button>
-        <div className="dropdown-content">
-          <h3 className="dropdown-title">Home Appliances We Repair</h3>
-          <div className="row">
-            <div className="column">
-              <a href="#"><img src="/svg/fridge.svg" alt="" /> Refrigerator</a>
-              <a href="#"><img src="/svg/freezer.svg" alt="" /> Freezer</a>
-              <a href="#"><img src="/svg/dishwasher.svg" alt="" /> Dishwasher</a>
-              <a href="#"><img src="/svg/dryer.svg" alt="" /> Dryer</a>
-              <a href="#"><img src="/svg/oven.svg" alt="" /> Oven</a>
+        {/* Home Dropdown */}
+        <div
+          className="dropdown"
+          onMouseEnter={() => handleMouseEnter("home")}
+          onMouseLeave={handleMouseLeave}
+        >
+          <button className="dropbtn">Home</button>
+          {activeDropdown === "home" && (
+            <div className="dropdown-content">
+              <h3 className="dropdown-title">Home Appliances We Repair</h3>
+              <div className="row">
+                <div className="column">
+                  <a href="#"><img src="/svg/fridge.svg" alt="" /> Refrigerator</a>
+                  <a href="#"><img src="/svg/freezer.svg" alt="" /> Freezer</a>
+                  <a href="#"><img src="/svg/dishwasher.svg" alt="" /> Dishwasher</a>
+                  <a href="#"><img src="/svg/dryer.svg" alt="" /> Dryer</a>
+                  <a href="#"><img src="/svg/oven.svg" alt="" /> Oven</a>
+                </div>
+                <div className="column">
+                  <a href="#">Microwave</a>
+                  <a href="#">Cooktop</a>
+                  <a href="#">Range Hood</a>
+                  <a href="#">Ice Machine</a>
+                  <a href="#">Slushie Machine</a>
+                </div>
+                <div className="column">
+                  <a href="#">Walk-in Cooler</a>
+                  <a href="#">Walk-in Freezer</a>
+                  <a href="#">Showcase Refrigerator</a>
+                </div>
+              </div>
             </div>
-            <div className="column">
-              <a href="#">Microwave</a>
-              <a href="#">Cooktop</a>
-              <a href="#">Range Hood</a>
-              <a href="#">Ice Machine</a>
-              <a href="#">Slushie Machine</a>
-            </div>
-            <div className="column">
-              <a href="#">Walk-in Cooler</a>
-              <a href="#">Walk-in Freezer</a>
-              <a href="#">Showcase Refrigerator</a>
-            </div>
-          </div>
+          )}
         </div>
-      </div>
 
-      {/* Phone Number */}
-      <a href="#top" className="phone-number">(323) 870-4790</a>
+        {/* Commercial Dropdown */}
+        <div
+          className="dropdown"
+          onMouseEnter={() => handleMouseEnter("commercial")}
+          onMouseLeave={handleMouseLeave}
+        >
+          <button className="dropbtn">Commercial</button>
+          {activeDropdown === "commercial" && (
+            <div className="dropdown-content">
+              <h3 className="dropdown-title">Commercial Appliances We Repair</h3>
+              <div className="row">
+                <div className="column">
+                  <a href="#"><img src="/svg/fridge.svg" alt="" /> Refrigerator</a>
+                  <a href="#"><img src="/svg/freezer.svg" alt="" /> Freezer</a>
+                  <a href="#"><img src="/svg/dishwasher.svg" alt="" /> Dishwasher</a>
+                  <a href="#"><img src="/svg/dryer.svg" alt="" /> Dryer</a>
+                  <a href="#"><img src="/svg/oven.svg" alt="" /> Oven</a>
+                </div>
+                <div className="column">
+                  <a href="#">Microwave</a>
+                  <a href="#">Cooktop</a>
+                  <a href="#">Range Hood</a>
+                  <a href="#">Ice Machine</a>
+                  <a href="#">Slushie Machine</a>
+                </div>
+                <div className="column">
+                  <a href="#">Walk-in Cooler</a>
+                  <a href="#">Walk-in Freezer</a>
+                  <a href="#">Showcase Refrigerator</a>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+
+        {/* Phone Number */}
+        <a href="#top" className="phone-number">(323) 870-4790</a>
+      </div>
     </div>
   );
 };
