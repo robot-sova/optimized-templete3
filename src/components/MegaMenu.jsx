b0857a6cc57bd221d0fda09a212593090b60d470
robot-sova <6195738869abysov@gmail.com>
2025-02-15 15:19:46 -0800

    1

--- src/components/MegaMenu.jsx
+++ src/components/MegaMenu.jsx
@@ -20,40 +20,11 @@ const MegaMenu = () => {
         <img alt="Logo" src="/logo.webp" width="157" height="70" />
       </a>
 
-      {/* Dropdown Menu */}
+      {/* Home Dropdown */}
       <div className="dropdown">
         <button className="dropbtn">Home</button>
         <div className="dropdown-content">
-          <h3 style={{ color: "#fff" }}>Home Appliances We Repair</h3>
-          <div className="row">
-            <div className="column">
-              <a href="#"><img src="/svg/fridge.svg" loading="lazy" alt="" width="14" height="14" /> Refrigerator</a>
-              <a href="#"><img src="/svg/freezer.svg" loading="lazy" alt="" width="14" height="14" /> Freezer</a>
-              <a href="#"><img src="/svg/dishwasher.svg" loading="lazy" alt="" width="14" height="14" /> Dishwasher</a>
-              <a href="#"><img src="/svg/dryer.svg" loading="lazy" alt="" width="14" height="14" /> Dryer</a>
-              <a href="#"><img src="/svg/oven.svg" loading="lazy" alt="" width="14" height="14" /> Oven</a>
-            </div>
-            <div className="column">
-              <a href="#">Microwave</a>
-              <a href="#">Cooktop</a>
-              <a href="#">Range Hood</a>
-              <a href="#">Ice Machine</a>
-              <a href="#">Slushie Machine</a>
-            </div>
-            <div className="column">
-              <a href="#">Walk-in Cooler</a>
-              <a href="#">Walk-in Freezer</a>
-              <a href="#">Showcase Refrigerator</a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      {/* More Dropdowns */}
-      <div className="dropdown">
-        <button className="dropbtn">Commercial</button>
-        <div className="dropdown-content">
-          <h3 style={{ color: "#fff" }}>Commercial Appliances We Repair</h3>
+          <h3 className="dropdown-title">Home Appliances We Repair</h3>
           <div className="row">
             <div className="column">
               <a href="#"><img src="/svg/fridge.svg" alt="" /> Refrigerator</a>
