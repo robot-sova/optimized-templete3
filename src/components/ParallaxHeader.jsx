c63af2946135fb75b7f270f1f203502e29d64008
robot-sova <6195738869abysov@gmail.com>
2025-02-15 18:59:15 -0800

    1

--- src/components/ParallaxHeader.jsx
+++ src/components/ParallaxHeader.jsx
@@ -5,8 +5,9 @@ const ParallaxHeader = () => {
   return (
     <header className="parallax-header">
       <div className="overlay">
-        <h1 className="fade-in">Same Day Dryer Repair</h1>
-        <p className="fade-in">Fast & Reliable Service</p>
+        <h1 className="fade-in">Same Day Appliance Repair</h1>
+        <p className="fade-in">Need fast, reliable appliance repair? Look no further! Our team offers same-day, expert repair services for all major home and commercial appliances, including refrigerators, freezers, washers, dryers, dishwashers, stoves, ovens, microwaves, ice machines, fireplaces, BBQ grills, cooktops, range hoods, walk-in coolers, HVAC systems, and more.<br></br>
+</p>
       </div>
     </header>
   );
