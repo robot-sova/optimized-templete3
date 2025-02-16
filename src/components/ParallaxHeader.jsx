50bd726c3475f957da5fe47f5415b32b8dfd9b0f
robot-sova <6195738869abysov@gmail.com>
2025-02-15 17:20:56 -0800

    1

---
+++ src/components/ParallaxHeader.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./ParallaxHeader.css";
+
+const ParallaxHeader = () => {
+  return (
+    <header className="parallax-header">
+      <div className="overlay">
+        <h1>Same Day Dryer Repair</h1>
+        <p>Fast & Reliable Service</p>
+      </div>
+    </header>
+  );
+};
+
+export default ParallaxHeader;
